6d97454d196cbc9312afa4d8bc4fb14503685de5
Zaim Zazali <zaim.zazali@gmail.com>
2020-05-30 18:49:20 +0100

    edit formatting

--- routes/pages/not_compatible/assets/js/not_compatible.js
+++ routes/pages/not_compatible/assets/js/not_compatible.js
@@ -1,36 +1,42 @@
-"use strict";
+/* eslint-disable strict */
 
-// "IE8 and Below" got some weird errors, so I tried with 'new' instead
-window.onload = new (function () {
-  start();
-})();
+'use strict';
 
-function start() {
-  setButtons();
+/* eslint-disable no-var */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+
+function openURL(urlString) {
+  window.open(urlString, '_blank');
 }
 
 function setButtons() {
   var theObj;
 
   // Chrome
-  theObj = document.getElementById("link_chrome");
+  theObj = document.getElementById('link_chrome');
   theObj.onclick = function () {
-    openURL("https://www.google.com/chrome/");
+    openURL('https://www.google.com/chrome/');
   };
 
   // Firefox
-  theObj = document.getElementById("link_firefox");
+  theObj = document.getElementById('link_firefox');
   theObj.onclick = function () {
-    openURL("https://www.mozilla.org/en-GB/firefox/new/");
+    openURL('https://www.mozilla.org/en-GB/firefox/new/');
   };
 
   // Opera
-  theObj = document.getElementById("link_opera");
+  theObj = document.getElementById('link_opera');
   theObj.onclick = function () {
-    openURL("https://www.opera.com/");
+    openURL('https://www.opera.com/');
   };
 }
 
-function openURL(urlString) {
-  window.open(urlString, "_blank");
+function start() {
+  setButtons();
 }
+
+// "IE8 and Below" got some weird errors, so I tried with 'new' instead
+window.onload = new (function () {
+  start();
+})();
